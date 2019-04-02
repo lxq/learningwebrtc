99713abd59c8705d8052c993c9e73c8249a70ef6
lxq <linxq@live.com>
2019-04-02 09:46:51 +0800

    捕捉视频的练习.

---
+++ photobooth.js
@@ -0,0 +1,47 @@
+// photobooth.js
+// 2019/4/2
+// 《Learning WebRTC》
+
+function getUserMedia() {
+    return (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+usermedia = getUserMedia();
+if (!!usermedia) {
+    navigator.getUserMedia = usermedia;
+    
+    var cons = {
+        audio: false,
+        video: true
+    }
+
+    var video = document.querySelector("video");
+    var canvas = document.querySelector("canvas");
+    var streaming = false;
+
+    navigator.getUserMedia(
+        cons,
+        // 正确的回调.
+        // @param stream 当前设备上能产生数据流的媒体设备.
+        function(stream) {
+            video.src = window.URL.createObjectURL(stream);
+            streaming = true;
+        },
+        // 错误回调.
+        function (err) {
+            console.log("获取媒体设备错误：", err);
+        }
+    );
+
+    document.querySelector("#capture").addEventListener("click", function(e) {
+        if (streaming) {
+            canvas.width = video.clientWidth;
+            canvas.height = video.clientHeight;
+
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(video, 0, 0);
+        }
+    });
+} else {
+    alert("浏览器不支持 getUserMedia!");
+}
