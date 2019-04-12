da8f21bc15890a21428d7d766c47b2d4c62f7e03
lxq <linxq@live.com>
2019-04-12 19:58:49 +0800

    修改了新API版本，但运行上存在问题。

--- exam03/main.js
+++ exam03/main.js
@@ -3,85 +3,102 @@
 // 《Learning WebRTC》
 // RTCPeerConnection 练习.
 
-function getUserMedia() {
-    return (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-}
+// 采用StreamMedia API重写.
+(function(){
+    var video_me = null, video_you = null;
+    var peer_me = null, peer_you = null;
 
-function hasPeerConn() {
-    window.RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-    return !!(window.RTCPeerConnection);
-}
+    function startup() {
+        video_me = document.getElementById("yours");
+        video_you = document.getElementById("theirs");
 
-var yv = document.querySelector("#yours");
-var tv = document.querySelector("#theirs");
-
-var yc, tc;
-
-var usermedia = getUserMedia();
-if (!!usermedia) {
-    navigator.getUserMedia = usermedia;
-    
-    var cons = {
-        audio: false,
-        video: true
+        navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then(function(stream) {
+                video_me.srcObject = stream;
+                start_peer(stream);
+            })
+            .catch(function(err) {
+                alert("获取媒体流失败：" + err);
+            });
     }
 
-    navigator.getUserMedia(
-        cons,
-        // 正确的回调.
-        // @param stream 当前设备上能产生数据流的媒体设备.
-        function(stream) {
-            yv.src = window.URL.createObjectURL(stream);
-            if (hasPeerConn()) {
-                startPeer(stream);
-            } else {
-                console.log("浏览器不支持 RTCPeerConnection!");
+    function start_peer(stream) {
+        var cfg = {
+            "iceServers":[{"url": "stun:127.0.0.1:9678"}]
+        };
+        peer_me = new RTCPeerConnection(cfg);
+        peer_you = new RTCPeerConnection(cfg);
+
+        // listen stream
+        peer_me.addStream(stream);
+        peer_you.onaddstream = function(obj) {
+            video_you.srcObject = obj.stream;
+        };
+
+        // ICE
+        peer_me.onicecandidate = function(e) {
+            if (e.candidate) {
+                peer_you.addIceCandidate(new RTCIceCandidate(e.candidate));
             }
-        },
-        // 错误回调.
-        function (err) {
-            console.log("获取媒体设备错误：", err);
-        }
-    );
+        };
+        peer_you.onicecandidate = function(e) {
+            if (e.candidate) {
+                peer_me.addIceCandidate(new RTCIceCandidate(e.candidate));
+            }
+        };
 
-} else {
-    alert("浏览器不支持 webRTC!");
-}
+        // offer
+        peer_me.createOffer(function(offer) {
+            peer_me.setLocalDescription(new RTCSessionDescription(offer));
+            peer_you.setRemoteDescription(new RTCSessionDescription(offer));
 
-function startPeer(stream) {
-    var cfg = {
-        "iceServers":[{"url": "stun:127.0.0.1:9678"}]
-    };
-    yc = new RTCPeerConnection(cfg);
-    tc = new RTCPeerConnection(cfg);
-
-    // 监听流
-    yc.addStream(stream);
-    tc.onaddstream = function(e) {
-        tv.src = window.URL.createObjectURL(e.stream);
-    };
-
-    // ICE
-    yc.onicecandidate = function(e) {
-        if (e.candidate) {
-            tc.addIceCandidate(new RTCIceCandidate(e.candidate));
-        }
-    };
-    tc.onicecandidate = function(e) {
-        if (e.candidate) {
-            yc.addIceCandidate(new RTCIceCandidate(e.candidate));
-        }
-    };
-
-    // offer
-    yc.createOffer(function(offer){
-        yc.setLocalDescription(offer);
-        tc.setRemoteDescription(offer);
-
-        tc.createAnswer(function(offer){
-            tc.setLocalDescription(offer);
-            yc.setRemoteDescription(offer);
+            peer_you.createAnswer(function(offer) {
+                peer_you.setLocalDescription(new RTCSessionDescription(offer));
+                peer_me.setRemoteDescription(new RTCSessionDescription(offer));
+            });
         });
-    });
+    }
 
-}
+    window.addEventListener("load", startup, false);
+    
+})();
+
+
+
+// function startPeer(stream) {
+//     var cfg = {
+//         "iceServers":[{"url": "stun:127.0.0.1:9678"}]
+//     };
+//     yc = new RTCPeerConnection(cfg);
+//     tc = new RTCPeerConnection(cfg);
+
+//     // 监听流
+//     yc.addStream(stream);
+//     tc.onaddstream = function(e) {
+//         tv.src = window.URL.createObjectURL(e.stream);
+//     };
+
+//     // ICE
+//     yc.onicecandidate = function(e) {
+//         if (e.candidate) {
+//             tc.addIceCandidate(new RTCIceCandidate(e.candidate));
+//         }
+//     };
+//     tc.onicecandidate = function(e) {
+//         if (e.candidate) {
+//             yc.addIceCandidate(new RTCIceCandidate(e.candidate));
+//         }
+//     };
+
+//     // offer
+//     yc.createOffer(function(offer){
+//         yc.setLocalDescription(offer);
+//         tc.setRemoteDescription(offer);
+
+//         tc.createAnswer(function(offer){
+//             tc.setLocalDescription(offer);
+//             yc.setRemoteDescription(offer);
+//         });
+//     });
+
+// }
